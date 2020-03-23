367fb939736ada3fab301b15c596635323fff75f
D3TONAT0R <gdv@swissonline.com>
2020-03-23 17:28:51 +0100

    added StockAPI

---
+++ src/main/java/com/d3t/klplugin/StockMarketHandler.java
@@ -0,0 +1,9 @@
+package com.d3t.klplugin;
+
+import com.google.common.base.Ticker;
+
+public class StockMarketHandler {
+	public static float getStockValue(String tickerName) {
+		Ticker t = new Ticker()
+	}
+}
